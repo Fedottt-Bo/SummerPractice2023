5a86418d8fce38a1a4eb00707d1d2bc5ec9d983d
Fedottt-Bo <fedorbborodulin@gmail.com>
2023-06-02 10:11:06 +0300

    Moved all similar index files to one in root folder. Updated server for working with this format.

--- server.js
+++ server.js
@@ -14,25 +14,27 @@ app.use('/', express.static(__dirname + '/index'));
 app.use(favicon(__dirname + '/index/index_icon.ico'));
 
 // Initialize project pages array
+let ProjectIndices = new Set();
 let ProjectPages =
 [
-  ["/Fractal/Mondel1/", "Mondelbrot"],
-  ["/Fractal/Mondel2/", "Julia"],
-  ["/Fractal/Newton1/", "Newton Classic (3rd power)"],
-  ["/Fractal/Newton2/", "Newton Classic (5rd power)"],
-  ["/Fractal/Newton3/", "Newton Classic (13rd power)"],
-  ["/Fractal/Newton4/", "Newton Sin"],
+  ["/Fractal/Mondel.html", "?path=/Fractal/Mondel1/", "/Fractal/Mondel1/", "Mondelbrot"],
+  ["/Fractal/Mondel.html", "?path=/Fractal/Mondel2/", "/Fractal/Mondel2/", "Julia"],
+  ["/Fractal/Newton.html", "?path=/Fractal/Newton1/", "/Fractal/Newton1/", "Newton Classic (3rd power)"],
+  ["/Fractal/Newton.html", "?path=/Fractal/Newton2/", "/Fractal/Newton2/", "Newton Classic (5rd power)"],
+  ["/Fractal/Newton.html", "?path=/Fractal/Newton3/", "/Fractal/Newton3/", "Newton Classic (13rd power)"],
+  ["/Fractal/Newton.html", "?path=/Fractal/Newton4/", "/Fractal/Newton4/", "Newton Sin"],
 ]
-let ProjectPagesJSON = JSON.stringify(ProjectPages.map(val => { return [val[0] + 'index.html', val[1]]; }))
+let ProjectPagesJSON = JSON.stringify(ProjectPages.map(val => { ProjectIndices.add(val[0]); return [val[0] + val[1], val[3]]; }))
 
 // Add other pages
-ProjectPages.forEach((val, ind, arr) => app.use(val[0], express.static(__dirname + val[0])));
+ProjectPages.forEach((val, ind, arr) => app.use(val[2], express.static(__dirname + val[2])));
+ProjectIndices.forEach((val, ind, arr) => app.get(`${val}*`, (req, res) => res.sendFile(__dirname + val)));
 
 // Override pages list
-app.get('/list.txt', (req, res) => {  
+app.get('/list.txt*', (req, res) => {  
   res.writeHead(200);
   res.end(ProjectPagesJSON);
-})
+});
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
