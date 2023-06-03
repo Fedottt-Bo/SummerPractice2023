8973e45b0173f55759923651698f6bd6f59c7317
Fedottt-Bo <fedorbborodulin@gmail.com>
2023-06-03 12:09:58 +0300

    Extended webpack config.

--- WebGPU/resources/005_textured_model/webpack.config.js
+++ WebGPU/resources/005_textured_model/webpack.config.js
@@ -1,11 +1,33 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './main.js',
   mode: 'production',
   optimization: {
     chunkIds: 'total-size',
-    
+    innerGraph: true,
+    concatenateModules: true,
+    mangleWasmImports: true,
+    mergeDuplicateChunks: true,
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          compress: {
+            passes: 4,
+            unsafe_math: true,
+          },
+          ecma: undefined,
+          enclose: false,
+          ie8: true,
+          keep_classnames: false,
+          keep_fnames: false,
+          mangle: true,
+        },
+      }),
+    ],
   },
   output:
   {
